4604c4a0f0922cf99f1704cd90559d278e59f645
Mai Thành Hải Quân <maithanhhaiquan2004@gmail.com>
2025-12-22 16:44:07 +0700

    first commit

---
+++ web-admin/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className="flex justify-center items-center">Hello world!!!</p>
+    </div>
+  );
+}
+
+export default App;
